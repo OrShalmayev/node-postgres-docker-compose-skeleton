b20d40917f5d13c386e3366453bf177dd31c92b2
Or Shalmayev <shalmayev1993@gmail.com>
2020-12-16 00:02:12 +0200

    added packages and changed stuff

--- init.sql
+++ init.sql
@@ -1,1 +1,9 @@
 -- paste your PostgresSQL schema creation script here 
+CREATE DATABASE IF NOT EXISTS coralogix;
+USE coralogix;
+CREATE TABLE sessions(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    data json DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+    ended_at TIMESTAMP 
+);
